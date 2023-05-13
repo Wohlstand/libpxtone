d0c363d1f57ef2318bb91f2f72382d2dfed44e1f
Wohlstand <admin@wohlnet.ru>
2023-05-13 18:34:50 +0300

    pxtnPulse_Noise.cpp: Fixed the list comparison
    Here was an invalid code where the comparison to self was been. Because of that, this whole check of the loop always give the false result.

--- src/pxtnPulse_Noise.cpp
+++ src/pxtnPulse_Noise.cpp
@@ -372,8 +372,8 @@ int32_t pxtnPulse_Noise::Compare     ( const pxtnPulse_Noise *p_src ) const
 
 		for( int32_t e = 0; e < _units[ u ].enve_num; e++ )
 		{
-			if( _units[ u ].enves[ e ].x != _units[ u ].enves[ e ].x ) return 1;
-			if( _units[ u ].enves[ e ].y != _units[ u ].enves[ e ].y ) return 1;
+			if( p_src->_units[ u ].enves[ e ].x != _units[ u ].enves[ e ].x ) return 1;
+			if( p_src->_units[ u ].enves[ e ].y != _units[ u ].enves[ e ].y ) return 1;
 		}
 	}
 	return 0;
